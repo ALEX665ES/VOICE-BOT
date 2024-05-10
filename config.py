7af67b372e527d5dad0155fbf6d4af9b11a922aa
Ａｌｅｘ <127571445+ALEX665ES@users.noreply.github.com>
2024-05-10 07:42:08 +0530

    Update config.py

--- config.py
+++ config.py
@@ -33,7 +33,7 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/THE-VIP-BOY-OP/VIP-MUSIC",
+    "https://github.com/ALEX665ES/VIP-MUSICS",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "master")
 GIT_TOKEN = getenv(
